59339b4e737d0ac75ad011256d8667d8e5c3152a
Christalee Bieber <cbieber@alum.mit.edu>
2026-05-29 16:34:17 -0400

    selects & plays multiple files

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [files, setFiles] = useState<File[]>([]);
+  const [playing, setPlaying] = useState('');
+
+  const handleFileSelect = (e) => {
+    const filesArray: File[] = Array.from(e.target.files);
+    setFiles(filesArray);
+    setPlaying(window.URL.createObjectURL(filesArray[0]));
+  }
+
+  const handleEndOfPlay = () => {
+    if (files.length > 0) {
+      window.URL.revokeObjectURL(playing)
+      setFiles((prevFiles) => prevFiles.slice(1));
+      setPlaying(window.URL.createObjectURL(files[1]))
+    }
+  }
 
   return (
     <>
       <section id="center">
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        {files.length > 0 && (<audio controls autoPlay src={playing} onEnded={handleEndOfPlay}></audio>)}
+        <input type="file" name="files" accept="audio/*" multiple onChange={handleFileSelect} />
+        <ul>
+          {files.length > 0 && files.map((file, index) => (<li key={`${file.name}-${index}`}>{file.name}</li>))}
+        </ul>
       </section>
-
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
