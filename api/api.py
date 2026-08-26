acc62a7a2c1f9ca8580b4fb1e4b799e33b91e97b
Christalee Bieber <cbieber@alum.mit.edu>
2026-08-26 15:02:09 -0400

    add flake8 config

--- api/api.py
+++ api/api.py
@@ -8,8 +8,10 @@ AUDIO_EXTENSIONS = ["*.mp3", "*.ogg", "*.flac", "*.wav", "*.m4a"]
 
 @app.route("/api/library", methods=["POST"])
 def get_library():
-    libraryPath = request.get_json()["library"]
-    p = Path(libraryPath)
-    matched = [f for f in p.rglob("*") if any([f.match(ext) for ext in AUDIO_EXTENSIONS])]
-    files = list(map(Path.as_uri, matched))
-    return {"files": files}
+  libraryPath = request.get_json()["library"]
+  p = Path(libraryPath)
+  matched = [
+      f for f in p.rglob("*") if any([f.match(ext) for ext in AUDIO_EXTENSIONS])
+  ]
+  files = list(map(Path.as_uri, matched))
+  return {"files": files}
